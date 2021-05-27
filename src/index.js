092f5b5312139701676dc43a08d70347a384967d
wb-lx652828 <wb-lx652828@alibaba-inc.com>
2021-05-27 15:47:32 +0800

    初始化react脚手架

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+
+// 页面性能分析文件 需要web-vitals的支持
 import reportWebVitals from './reportWebVitals';
 
+// App 被 React.StrictMode 包裹后, 会对APP以及其所有的子组件进行检查是否合理(比如已经被弃用了的会警告提示)
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+
+// setupTest.js 组件单元测试的文件 需要jest-dom的支持  也可以做应用的整体测试
 import '@testing-library/jest-dom';
+
